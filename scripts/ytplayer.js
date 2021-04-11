0ae777124f5f18976d1d704aff15fbaa6e6cd120
Sergio Nieto <s.nieto.t@gmail.com>
2021-04-10 21:07:39 -0700

    Background Video Implementation
    This sets the foundation for the website. Users shouldn't be able
    to interact with the player and it should be responsive to different
    window sizes.

---
+++ scripts/ytplayer.js
@@ -0,0 +1,21 @@
+var player;
+var onYouTubeIframeAPIReady = () => {
+    player = new YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: 'IlhbR0B0fQA',
+        playerVars: {
+            'autoplay': 1,
+            'mute':1,
+            'controls': 0,
+            'disablekb': 1,
+            'fs': 0,
+            'modestbranding': 1,
+            'start': 11,
+            'playsinline': 1,
+            'rel': 0,
+            'enablejsapi': 1,
+            'vq': 'hd1080'
+        }
+    });
+}
